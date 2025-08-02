4c6dff11287b1d029e9406f43015df774ab7d4c7
falcao <amine.saguer1@gmail.com>
2025-08-02 18:55:04 +0100

    inital commit: setting up the project with basic files and structure, tailwind, shadcn/ui, and next.js

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-9xl font-bold">Hello World</h1>
+    </div>
+  );
+}
